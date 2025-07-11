e8f27e084710c4f691010e95f72abde2502c6861
hamzah <shaikhhamzah93@gmail.com>
2025-07-11 23:47:12 +0530

    Home page and Root Layout

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "blog_generator"
 
 urlpatterns = [
-    path('', view=views.index, name='index')
+    path('', view=views.index, name='home'),
+    path('login', view=views.user_login, name='login'),
+    path('logout', view=views.user_logout, name='logout'),
+    path('register', view=views.user_register, name='register')
 ]
--- blog_generator/views.py
+++ blog_generator/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def user_login(request):
+    pass
+
+def user_logout(request):
+    pass
+
+def user_register(request):
+    pass
