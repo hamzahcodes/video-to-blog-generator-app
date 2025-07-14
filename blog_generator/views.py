88a78e4587a33dc00b3580f608791c371ab87a44
hamzah <shaikhhamzah93@gmail.com>
2025-07-14 22:40:11 +0530

    authentication | authorization

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,15 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, 'index.html')
 
 def user_login(request):
-    pass
 
-def user_logout(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid Credentials'
+            return render(request, 'login.html', { 'error_message': error_message })
+        
+    return render(request, 'login.html')
+
 
 def user_register(request):
-    pass
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeat_password = request.POST['repeatPassword']
+
+        if password == repeat_password:
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                login(request, user)
+                return redirect('/')
+            
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'signup.html', { 'error_message' : error_message })
+            
+        else:
+            error_message: str = 'Passwords do not match'
+            return render(request, 'signup.html', { 'error_message' : error_message })
+    
+
+    return render(request, 'signup.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
